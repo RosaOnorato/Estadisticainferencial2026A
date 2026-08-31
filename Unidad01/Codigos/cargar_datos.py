c73ad9cd725875488d7e9bf99a825ba4589ca565
RosaOnorato <cardonaonoratoros@gmail.com>
2026-08-31 13:54:37 -0600

    Add data loading functionality in cargar_datos.py

---
+++ Unidad01/Codigos/cargar_datos.py
@@ -0,0 +1,4 @@
+import pandas as pd
+links_csv = "https://raw.githubusercontent.com/RosaOnorato/Estad-sticainferencial2026A/refs/heads/main/Unidad01/Problemario/datos_problema_01.csv"
+df = pd.read_csv(links_csv)
+df
